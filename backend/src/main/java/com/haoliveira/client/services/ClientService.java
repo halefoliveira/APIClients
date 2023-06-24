769b6d150e23951d0be6e08ce2f985d384f4733b
Halef Oliveira <halef.alves@gmail.com>
2023-06-23 22:29:30 -0300

    Implemmentação do put e update

--- backend/src/main/java/com/haoliveira/client/services/ClientService.java
+++ backend/src/main/java/com/haoliveira/client/services/ClientService.java
@@ -13,25 +13,55 @@ import com.haoliveira.client.entities.Client;
 import com.haoliveira.client.repositories.ClientRepository;
 import com.haoliveira.client.services.exceptions.EntityNotFoundExceptions;
 
-
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll(){
+	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 
 	}
+
 	@Transactional(readOnly = true)
 	public ClientDTO findId(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(()-> new EntityNotFoundExceptions("Entity not Found"));
+		Client entity = obj.orElseThrow(() -> new EntityNotFoundExceptions("Entity not Found"));
 		return new ClientDTO(entity);
 	}
-		
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getReferenceById(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+			
+		} catch (EntityNotFoundException e) {
+			throw new EntityNotFoundExceptions("Id not found" + id);
+		}
+
+	}
 }
